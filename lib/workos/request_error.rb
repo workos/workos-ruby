b084f626f16b37729f2bd806446eabf56c29819f
Sam Bauch <sam@workos.com>
2019-12-11 15:08:16 -0500

    v0.0.1 release on Rubygems with SSO Support

---
+++ lib/workos/request_error.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+module WorkOS
+  # Raised when an API request is unsuccessful
+  class RequestError < StandardError; end
+end
--- lib/workos/version.rb
+++ lib/workos/version.rb
@@ -1,6 +1,7 @@
-# typed: strong
 # frozen_string_literal: true
+# typed: true
+
 
 module WorkOS
-  VERSION = '0.0.0.pre'
+  VERSION = '0.0.1'
 end
