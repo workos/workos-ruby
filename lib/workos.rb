1d43aa7f8406a6c95efce9882a7d6a9dd9878b05
Mark Tran <mark.tran@gmail.com>
2020-04-07 22:19:42 -0400

    update env var (#27)
    * update env var
    
    * update comment

--- lib/workos.rb
+++ lib/workos.rb
@@ -6,7 +6,7 @@ require 'sorbet-runtime'
 
 # Use the WorkOS module to authenticate your
 # requests to the WorkOS API. The gem will read
-# your API key automatically from the ENV var `WORKOS_KEY`.
+# your API key automatically from the ENV var `WORKOS_API_KEY`.
 # Alternatively, you can set the key yourself with
 # `WorkOS.key = [your api key]` somewhere in the load path of
 # your application, such as an initializer.
@@ -37,5 +37,8 @@ module WorkOS
   autoload :AuthenticationError, 'workos/errors'
   autoload :InvalidRequestError, 'workos/errors'
 
-  WorkOS.key = ENV['WORKOS_KEY'] unless ENV['WORKOS_KEY'].nil?
+  # Remove WORKOS_KEY at some point in the future. Keeping it here now for
+  # backwards compatibility.
+  key = ENV['WORKOS_API_KEY'] || ENV['WORKOS_KEY']
+  WorkOS.key = key unless key.nil?
 end
