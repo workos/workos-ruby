68592309c8e6b2cad52c952b3c40b3f78e4ae981
Rohan Jadvani <rohan@workos.com>
2021-04-07 12:32:39 -0500

    feat(workos-ruby): Add `connection` to `authorization_url`

--- spec/lib/workos/sso_spec.rb
+++ spec/lib/workos/sso_spec.rb
@@ -73,7 +73,41 @@ describe WorkOS::SSO do
       end
     end
 
-    context 'with neither domain or provider' do
+    context 'with a connection' do
+      let(:args) do
+        {
+          connection: 'connection_123',
+          client_id: 'workos-proj-123',
+          redirect_uri: 'foo.com/auth/callback',
+          state: {
+            next_page: '/dashboard/edit',
+          }.to_s,
+        }
+      end
+      it 'returns a valid URL' do
+        authorization_url = described_class.authorization_url(**args)
+
+        expect(URI.parse(authorization_url)).to be_a URI
+      end
+
+      it 'returns the expected hostname' do
+        authorization_url = described_class.authorization_url(**args)
+
+        expect(URI.parse(authorization_url).host).to eq(WorkOS::API_HOSTNAME)
+      end
+
+      it 'returns the expected query string' do
+        authorization_url = described_class.authorization_url(**args)
+
+        expect(URI.parse(authorization_url).query).to eq(
+          'client_id=workos-proj-123&redirect_uri=foo.com%2Fauth%2Fcallback' \
+          '&response_type=code&state=%7B%3Anext_page%3D%3E%22%2Fdashboard%2F' \
+          'edit%22%7D&connection=connection_123',
+        )
+      end
+    end
+
+    context 'with neither connection, domain, or provider' do
       let(:args) do
         {
           client_id: 'workos-proj-123',
@@ -88,7 +122,7 @@ describe WorkOS::SSO do
           described_class.authorization_url(**args)
         end.to raise_error(
           ArgumentError,
-          'Either domain or provider is required.',
+          'Either connection, domain, or provider is required.',
         )
       end
     end
