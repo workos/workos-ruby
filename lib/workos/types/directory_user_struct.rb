3dfc305f5e2d2089384d810a1ff31e72c4d2a6a8
Blair Lunceford <blair@workos.com>
2021-06-04 12:50:16 -0600

    Support groups attribute for directory user

--- lib/workos/types/directory_user_struct.rb
+++ lib/workos/types/directory_user_struct.rb
@@ -13,6 +13,7 @@ module WorkOS
       const :last_name, T.nilable(String)
       const :username, T.nilable(String)
       const :state, T.nilable(String)
+      const :groups, T::Array[T.untyped]
       const :raw_attributes, T::Hash[Symbol, Object]
     end
   end
