b2e04defd17ffe9770517a2c8ca29be3b742c930
Alex Slepak <alex@workos.com>
2022-04-15 15:15:38 -0700

    include hash_provider

--- lib/hash_provider.rb
+++ lib/hash_provider.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 # typed: true
 
-# Module to include an explicit method for converting a model into a Hash containing
-# its attributes. Default implementation will simply call to_json. Individual classes
-# may override.
-module HashProvider
-  def to_h
-    to_json
+module WorkOS
+  # Module to include an explicit method for converting a model into a Hash containing
+  # its attributes. Default implementation will simply call to_json. Individual classes
+  # may override.
+  module HashProvider
+    def to_h
+      to_json
+    end
   end
 end
