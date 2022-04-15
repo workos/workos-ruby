73673971cafac76f95aa3a3e32a0f360a5cb84d5
Alex Slepak <alex@workos.com>
2022-04-15 15:37:34 -0700

    include kernel to give sorbet access to raise

--- lib/hash_provider.rb
+++ lib/hash_provider.rb
@@ -6,6 +6,8 @@ module WorkOS
   # its attributes. Default implementation will simply call to_json. Individual classes
   # may override.
   module HashProvider
+    include Kernel
+    
     def to_json
       raise "Must be implemented by including class."
     end
