116455d65fed92b77abaa6f2205a6ce6885f622e
Alex Slepak <alex@workos.com>
2022-04-15 15:02:32 -0700

    include new file

---
+++ lib/hash_provider.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# typed: true
+
+# Module to include an explicit method for converting a model into a Hash containing
+# its attributes. Default implementation will simply call to_json. Individual classes
+# may override.
+module HashProvider
+  def to_h
+    to_json
+  end
+end
