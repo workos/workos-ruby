9e6436ae933d0b762673c9f6739072a8e92250c4
Blair Lunceford <74257063+blairlunceford@users.noreply.github.com>
2022-06-13 10:29:24 -0600

    Add getter method for directory user primary email (#161)
    * Add getter method for directory user primary email
    
    * Apply suggested changes
    
    * Use find over each to retrive primary email
    
    * Apply suggested edits to shorten method

---
+++ spec/lib/workos/directory_user_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+# typed: false
+
+describe WorkOS::DirectoryUser do
+  # rubocop:disable Layout/LineLength
+  describe '.get_primary_email' do
+    context 'with one primary email' do
+      it 'returns the primary email' do
+        user = WorkOS::DirectoryUser.new('{"object":"directory_user","id":"directory_user_01FAZYNPC8M0HRYTKFP2GNX852","directory_id":"directory_01FAZYMST676QMTFN1DDJZZX87","idp_id":"6092c280a3f1e19ef6d8cef8","username":"logan@workos.com","emails":[{"primary":true,"value":"logan@workos.com"}, {"primary":false,"value":"logan@gmail.com"}],"first_name":"Logan","last_name":"Gingerich","state":"active","raw_attributes":{},"custom_attributes":{},"groups":[]}')
+        expect(user.primary_email).to eq('logan@workos.com')
+      end
+    end
+
+    context 'with multiple primary emails' do
+      it 'returns the first email marked as primary' do
+        user = WorkOS::DirectoryUser.new('{"object":"directory_user","id":"directory_user_01FAZYNPC8M0HRYTKFP2GNX852","directory_id":"directory_01FAZYMST676QMTFN1DDJZZX87","idp_id":"6092c280a3f1e19ef6d8cef8","username":"logan@workos.com","emails":[{"primary":true,"value":"logan@workos.com"}, {"primary":true,"value":"logan@gmail.com"}],"first_name":"Logan","last_name":"Gingerich","state":"active","raw_attributes":{},"custom_attributes":{},"groups":[]}')
+        expect(user.primary_email).to eq('logan@workos.com')
+      end
+    end
+
+    context 'with no primary emails' do
+      it 'returns nil' do
+        user = WorkOS::DirectoryUser.new('{"object":"directory_user","id":"directory_user_01FAZYNPC8M0HRYTKFP2GNX852","directory_id":"directory_01FAZYMST676QMTFN1DDJZZX87","idp_id":"6092c280a3f1e19ef6d8cef8","username":"logan@workos.com","emails":[{"primary":false,"value":"logan@gmail.com"}],"first_name":"Logan","last_name":"Gingerich","state":"active","raw_attributes":{},"custom_attributes":{},"groups":[]}')
+        expect(user.primary_email).to eq(nil)
+      end
+    end
+
+    context 'with an empty email array' do
+      it 'returns nil' do
+        user = WorkOS::DirectoryUser.new('{"object":"directory_user","id":"directory_user_01FAZYNPC8M0HRYTKFP2GNX852","directory_id":"directory_01FAZYMST676QMTFN1DDJZZX87","idp_id":"6092c280a3f1e19ef6d8cef8","username":"logan@workos.com","emails":[],"first_name":"Logan","last_name":"Gingerich","state":"active","raw_attributes":{},"custom_attributes":{},"groups":[]}')
+        expect(user.primary_email).to eq(nil)
+      end
+    end
+  end
+  # rubocop:enable Layout/LineLength
+end
