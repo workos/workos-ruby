cefce389786b6ca57958cb4ac161aaf0c575d95f
Blair Lunceford <blair@workos.com>
2022-08-11 17:03:28 -0600

    Add tests for setting the timeouts in the client

--- spec/support/shared_examples/client_spec.rb
+++ spec/support/shared_examples/client_spec.rb
@@ -13,4 +13,18 @@ RSpec.shared_examples 'client' do
   it 'returns new instance' do
     expect(described_class.client.object_id).to_not eq described_class.client.object_id
   end
+
+  if RUBY_VERSION >= '2.6.0'
+    it 'sets the timeouts, including the write timeout' do
+      expect(described_class.client.open_timeout).to_not be_nil
+      expect(described_class.client.read_timeout).to_not be_nil
+      expect(described_class.client.write_timeout).to_not be_nil
+    end
+  else
+    it 'sets the open and read timeouts, but not the write timeout' do
+      expect(described_class.client.open_timeout).to_not be_nil
+      expect(described_class.client.read_timeout).to_not be_nil
+      expect(described_class.client.write_timeout).to be_nil
+    end
+  end
 end
