e6388aac9d2c7b3a0bb347140d25b348255d03e3
Will Duffy <willman@workos.com>
2023-08-26 10:49:41 -0400

    Bring User in line with current API (#217)
    * Rename email_verified_at to email_verified and change it to a boolean.
    * Remove all *_profile_id fields.
    * Remove user_type field from User.
    * Remove type argument from list_users.

--- lib/workos/types/user_struct.rb
+++ lib/workos/types/user_struct.rb
@@ -9,10 +9,7 @@ module WorkOS
       const :email, String
       const :first_name, T.nilable(String)
       const :last_name, T.nilable(String)
-      const :email_verified_at, T.nilable(String)
-      const :google_oauth_profile_id, T.nilable(String)
-      const :sso_profile_id, T.nilable(String)
-      const :user_type, String
+      const :email_verified, T::Boolean
       const :created_at, String
       const :updated_at, String
     end
