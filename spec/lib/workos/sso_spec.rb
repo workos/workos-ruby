027306416b30fe86f268b56e9bf7828760503c89
Laura Beatris <48022589+LauraBeatris@users.noreply.github.com>
2023-11-19 14:29:51 -0300

    Remove duplicated `domain` test block

--- spec/lib/workos/sso_spec.rb
+++ spec/lib/workos/sso_spec.rb
@@ -108,40 +108,6 @@ describe WorkOS::SSO do
       end
     end
 
-    context 'with a domain' do
-      let(:args) do
-        {
-          domain: 'foo.com',
-          client_id: 'workos-proj-123',
-          redirect_uri: 'foo.com/auth/callback',
-          state: {
-            next_page: '/dashboard/edit',
-          }.to_s,
-        }
-      end
-      it 'returns a valid URL' do
-        authorization_url = described_class.authorization_url(**args)
-
-        expect(URI.parse(authorization_url)).to be_a URI
-      end
-
-      it 'returns the expected hostname' do
-        authorization_url = described_class.authorization_url(**args)
-
-        expect(URI.parse(authorization_url).host).to eq(WorkOS.config.api_hostname)
-      end
-
-      it 'returns the expected query string' do
-        authorization_url = described_class.authorization_url(**args)
-
-        expect(URI.parse(authorization_url).query).to eq(
-          'client_id=workos-proj-123&redirect_uri=foo.com%2Fauth%2Fcallback' \
-          '&response_type=code&state=%7B%3Anext_page%3D%3E%22%2Fdashboard%2F' \
-          'edit%22%7D&domain=foo.com',
-        )
-      end
-    end
-
     context 'with a domain_hint' do
       let(:args) do
         {
