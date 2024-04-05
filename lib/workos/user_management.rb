1c4959ab66d50016785d9569fc11bfe2b9843fda
Amadeo Casas <amadeo@workos.com>
2024-04-05 10:55:20 -0400

    Add 'password_hash' and 'password_hash_type' to User creation

--- lib/workos/user_management.rb
+++ lib/workos/user_management.rb
@@ -163,9 +163,11 @@ module WorkOS
       # @param [String] first_name The user's first name.
       # @param [String] last_name The user's last name.
       # @param [Boolean] email_verified Whether the user's email address was previously verified.
+      # @param [String] password_hash The user's hashed password.
+      # @option [String] password_hash_type The algorithm originally used to hash the password.
       #
       # @return [WorkOS::User]
-      def create_user(email:, password: nil, first_name: nil, last_name: nil, email_verified: nil)
+      def create_user(email:, password: nil, first_name: nil, last_name: nil, email_verified: nil, password_hash: nil, password_hash_type: nil)
         request = post_request(
           path: '/user_management/users',
           body: {
@@ -174,6 +176,8 @@ module WorkOS
             first_name: first_name,
             last_name: last_name,
             email_verified: email_verified,
+            password_hash: password_hash,
+            password_hash_type: password_hash_type,
           },
           auth: true,
         )
