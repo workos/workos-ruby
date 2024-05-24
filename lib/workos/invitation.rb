0d4956de7bdd843653edc8e7336c1fe51c63269c
Blair Lunceford <74257063+blairlunceford@users.noreply.github.com>
2024-05-24 14:19:44 -0600

    Add methods and events for email verification and password reset (#298)

--- lib/workos/invitation.rb
+++ lib/workos/invitation.rb
@@ -8,8 +8,9 @@ module WorkOS
     include HashProvider
 
     attr_accessor :id, :email, :state, :accepted_at, :revoked_at, :accept_invitation_url,
-                  :expires_at, :token, :organization_id, :created_at, :updated_at
+                  :expires_at, :token, :organization_id, :inviter_user_id, :created_at, :updated_at
 
+    # rubocop:disable Metrics/AbcSize
     def initialize(json)
       hash = JSON.parse(json, symbolize_names: true)
 
@@ -19,12 +20,14 @@ module WorkOS
       @token = hash[:token]
       @accept_invitation_url = hash[:accept_invitation_url]
       @organization_id = hash[:organization_id]
+      @inviter_user_id = hash[:inviter_user_id]
       @accepted_at = hash[:accepted_at]
       @revoked_at = hash[:revoked_at]
       @expires_at = hash[:expires_at]
       @created_at = hash[:created_at]
       @updated_at = hash[:updated_at]
     end
+    # rubocop:enable Metrics/AbcSize
 
     def to_json(*)
       {
@@ -34,6 +37,7 @@ module WorkOS
         token: token,
         accept_invitation_url: accept_invitation_url,
         organization_id: organization_id,
+        inviter_user_id: inviter_user_id,
         accepted_at: accepted_at,
         revoked_at: revoked_at,
         expires_at: expires_at,
