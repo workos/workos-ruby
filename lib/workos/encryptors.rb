d16828b91710373f0b2eeb11b2c7d40783daadd2
ci <ci@workos.com>
2026-04-15 15:05:23 -0400

    wip: snapshot for persistence check

--- lib/workos/encryptors.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module WorkOS
-  # Encryptors module provides pluggable encryption implementations for session data.
-  # The default encryptor is AesGcm, which uses AES-256-GCM encryption.
-  module Encryptors
-    autoload :AesGcm, "workos/encryptors/aes_gcm"
-  end
-end
